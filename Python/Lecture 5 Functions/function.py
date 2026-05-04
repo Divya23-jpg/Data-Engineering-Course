77b6ebdeab8c91475e71d007b8b393aed2c198d1
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-04 18:33:46 +0530

    divya

--- Python/Lecture 5 Functions/function.py
+++ Python/Lecture 5 Functions/function.py
@@ -2,8 +2,19 @@
 Syntax:
 
 def function_name(parameter):
-    / statements
+    
+    / body of function
     return expression
 
 
 """
+
+# ! Write a function to check number is even or odd
+
+def evenodd(x):
+    if(x%2==0):
+        return "Even"
+    else:
+        return "Odd"
+    
+print(evenodd(10))
