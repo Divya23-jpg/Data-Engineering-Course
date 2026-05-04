a0e7b319535399d56447ef7b795d7c9f236ee3ae
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-04 19:59:19 +0530

    divya

--- Python/Lecture 5 Functions/string_reverse.py
+++ Python/Lecture 5 Functions/string_reverse.py
@@ -57,11 +57,24 @@ for i in sentence:
 
 # ! WAF To convert Keys (dictionery) to upper
 def con_key():
-    dict={
-    'a':1,
-    'b':2
+    my_dict = {
+        'a': 1,
+        'b': 2
     }
-    new_dict={k.upper():v for k,vin dict.item()}
+    new_dict = {k.upper(): v for k, v in my_dict.items()}
+    print(new_dict)
 
 con_key()
 
+def keys_upper(test_dict):
+    # return {k.upper(): keys_upper() if isinstance (v ,dict) else v for k,v in test_dict.items()}
+    return {k.upper(): keys_upper(v) if isinstance(v, dict) else v for k, v in test_dict.items()}
+
+test_dict={
+    'a':1,
+    'b':{'b':2},
+    'c':3
+}
+
+result=keys_upper(test_dict)
+print(result)
