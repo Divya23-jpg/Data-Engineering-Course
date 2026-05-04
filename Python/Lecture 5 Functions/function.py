636646e52d652549a0ce9cb25267294a222936a6
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-04 18:42:15 +0530

    divya

--- Python/Lecture 5 Functions/function.py
+++ Python/Lecture 5 Functions/function.py
@@ -19,10 +19,24 @@ def evenodd(x):
     
 # print(evenodd(10))
 
-# ?Default argument Example
+# !Default argument Example
 def myfunc(x,y=30):
     print("X:",x)
     print("Y:",y)
 
-print(myfunc(50))
-print(myfunc(50,100))
+# print(myfunc(50))
+# print(myfunc(50,100))
+
+# ! Keyword Argument (Order not  matters)
+def student(fname,lname):
+    print(fname,lname)
+
+# student(fname="Divya",lname="Kumawat")
+# student(lname="Kumawat",fname="Divya")
+
+# ! Positional Argument (Order matters)
+def nameage(name,age):
+    print("Hy , I am" , name)
+    print("Hi , My age is " , age)
+
+nameage("Divya",22)
