0a0f0fb8c15343591794518af472dd550a72f944
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-04 19:14:29 +0530

    divya

--- Python/Lecture 5 Functions/string_reverse.py
+++ Python/Lecture 5 Functions/string_reverse.py
@@ -11,3 +11,16 @@ def rev(str):
 print(rev("Divya"))
 
 
+# ! WAP to calculate of Uppercase and lowercase character
+
+def cal_up_lo(str):
+    for i in range(0,len(str)+1):
+        lower=0
+        upper=0
+        if(str.isupper()):
+            lower=lower+1
+        else:
+            upper=upper+1
+
+    print("Upper Case count is:"upper)
+    print("Upper Case count is:"upper)
