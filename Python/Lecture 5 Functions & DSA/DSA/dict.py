28f1b331c12ca209f543a26bd636da9a2163921a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-05 19:52:37 +0530

    divya

---
+++ Python/Lecture 5 Functions & DSA/DSA/dict.py
@@ -0,0 +1,11 @@
+# user={
+#     "name":["Divya","Anu","Khushi"],
+#     "ID":[123,124,125],
+#     "age":[18,16,20]
+# }
+
+data=dict([('name','Divya'),('id',123),('age',20)])
+print(type(data))
+
+
+
