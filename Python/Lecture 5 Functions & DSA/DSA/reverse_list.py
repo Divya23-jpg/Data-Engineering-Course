69860376ca968bd8dc0e3e19a5fea277e60957e2
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-05 18:29:56 +0530

    divya

--- Python/Lecture 5 Functions & DSA/DSA/reverse_list.py
+++ Python/Lecture 5 Functions & DSA/DSA/reverse_list.py
@@ -1,11 +1,32 @@
 """
 list=[5,10,15,20,25]
 output:
-new_list=[25,20,15,10,5]
+list=[25,20,15,10,5]
 
 
 """
+# !With In built Function
+# list=[5,10,15,20,25]
+# list.reverse()
+# # print(list)
 
-list=[5,10,15,20,25]
+# # ! with reversed method
+# a=[30,40,50,60,70]
+# rev=list(reversed(a))
+# # print(rev)
 
-print(list)
+# # ! slicing
+# b=[5,10,15,20,25]
+# rever=b[::-1]
+# # print(rever)
+
+# ! Using Loops
+# a=[1,2,3,4,5]
+# i,j=0,len[a]-1
+# while i<j:
+#     a[i],a[j]=a[j],a[i]
+#     i=i+1
+#     j=j+1
+
+# print(a)
+
---
+++ Python/Lecture 5 Functions & DSA/DSA/set.py
@@ -0,0 +1,11 @@
+# ! Set que
+
+a={0,1,2,3,4,5}
+for i in range(0,len(a)):
+    print(i)
+
+# ?Or
+
+a={0,1,2,3,4,5}
+for i in a:
+    print(i)
