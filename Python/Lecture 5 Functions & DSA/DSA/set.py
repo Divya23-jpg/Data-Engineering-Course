b710f777b9af7f7dadbf3263f43fcfba49e19139
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-05 18:59:57 +0530

    divya

--- Python/Lecture 5 Functions & DSA/DSA/set.py
+++ Python/Lecture 5 Functions & DSA/DSA/set.py
@@ -36,3 +36,42 @@
 # s=(1,1,2,3,4)
 # print(list(s))
 
+
+# ! Iterate another method
+# num_set={1,2,3,4,5}
+# # ? value is index and value is value
+# for value , char in enumerate(num_set):
+#     print(char, end=" ")
+
+
+# ! Adding single and multiple element
+# a=set()
+# print(a)
+# a.add("Divya")  #Adding single element in a set
+# print(a)
+
+# a.update(["Anushree","Gourav","Manju"])  #Adding multiple element in a set
+# print(a)
+
+
+# ! Remove element
+
+# b=set([0,1,2,3,4,5])
+# print(" original value:", b)
+# b.pop()  #Removing Random element
+# print(b)
+
+# b.remove(1)  #Removing specific element
+# print(b)
+
+# b.discard(1) #Removing specific element if not present gives error
+# print(b)
+
+# ! Remove  all element in a set
+
+s=set([0,1,2,3,4,5])
+for i in range(len(s)):
+    s.remove(i)
+
+print(s)
+
