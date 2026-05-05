4e24d2cfe4f3bc337420a299e08c42f4f36c29a8
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-05 18:34:45 +0530

    divya

--- Python/Lecture 5 Functions & DSA/DSA/set.py
+++ Python/Lecture 5 Functions & DSA/DSA/set.py
@@ -1,11 +1,21 @@
+
 # ! Set que
 
-a={0,1,2,3,4,5}
-for i in range(0,len(a)):
-    print(i)
+# a={0,1,2,3,4,5}
+# for i in range(0,len(a)):
+#     print(i)
 
-# ?Or
+# # ?Or
 
-a={0,1,2,3,4,5}
-for i in a:
-    print(i)
+# a={0,1,2,3,4,5}
+# for i in a:
+#     print(i)
+
+
+# a="Divya"
+# new=set(a)
+# print(type(new))
+
+set1=set("Divya")
+print(type(set1))
+print(set1)
