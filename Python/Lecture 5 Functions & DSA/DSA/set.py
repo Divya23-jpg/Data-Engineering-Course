7550e433d69e75de17523453df10a8f433cc38be
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-05 18:39:02 +0530

    Divya

--- Python/Lecture 5 Functions & DSA/DSA/set.py
+++ Python/Lecture 5 Functions & DSA/DSA/set.py
@@ -1,5 +1,5 @@
 
-# ! Set que
+# ! Set Print
 
 # a={0,1,2,3,4,5}
 # for i in range(0,len(a)):
@@ -11,11 +11,24 @@
 # for i in a:
 #     print(i)
 
-
+# ! string to set
 # a="Divya"
 # new=set(a)
 # print(type(new))
+    # ? Or
+# set1=set("Divya")
+# print(type(set1))
+# print(set1)
 
-set1=set("Divya")
-print(type(set1))
-print(set1)
+# ! List to set
+
+# list=[1,2,1,3,4]
+# new=set(list)
+# print(new)  #! If we print in a new line than loop use
+
+
+# ! Set to list
+
+s={"Hy I Am  Divya"}
+li=list(s)
+print(li)
