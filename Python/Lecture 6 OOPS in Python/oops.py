1d8dc56cd2f916ad9697852f7be7ecc1f48696a2
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-06 19:39:12 +0530

    divya

---
+++ Python/Lecture 6 OOPS in Python/oops.py
@@ -0,0 +1,50 @@
+
+# ! Create an "Greeter" class which greets user as Good Morning,Good Evening or Good Night by the Username
+
+# from datetime import datetime
+
+# class Greeter:
+#     def __init__(self, name):
+#         # self.name=input("Your name:")
+#         self.name=name
+
+#     def greet(self):
+#         current_hour=datetime.now().hour
+#         if 5<= current_hour<12:
+#             greeting="Good morning"
+#         elif 12<= current_hour<18:
+#             greeting="Good Evening"     
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     greeter=Greeter("Divya")
+#     print(greeter.greet())
+
+#  ! Create a  class for vehicles which shows info like vehicle_name,vehicle_type,top_speed,models etc
+
+# class Vehicle:
+#     def __init__(self):
+#         self.v_name = input("Enter Vehicle Name: ")
+#         self.v_type = input("Enter Vehicle Type: ")
+#         self.top_speed = input("Enter Vehicle Top speed: ")
+#         self.model = input("Enter Vehicle model: ")
+
+# # create object without arguments
+# v = Vehicle()
+# print(v.v_name, v.v_type, v.top_speed, v.model)
+ 
+                    # ? Or
+# class Vehicle:
+#     def __init__(self):
+#         self.v_name = name
+#         self.v_type = ty_pe
+#         self.top_speed =speed
+#         self.model =model
+
+# # create object without arguments
+# v = Vehicle("BMW","4 Vehicle")
+# print(v.v_name, v.v_type, v.top_speed, v.model)
