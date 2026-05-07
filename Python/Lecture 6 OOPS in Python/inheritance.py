bcc09402e1aa1f938f35f245f8cbce36129b8a8d
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-07 18:12:40 +0530

    divya

---
+++ Python/Lecture 6 OOPS in Python/inheritance.py
@@ -0,0 +1,28 @@
+"""
+Inheritance allows new classes to inherit properties and methods
+from existing class to promote code use.
+
+Types:
+1 Single,
+2 Multilevel
+3 Multiple inheritance
+
+
+"""
+
+
+
+# ! 
+
+class Transport:
+    def start(self):
+        print("Start Transport......")
+
+
+class Car(Transport):
+    def drive(self):
+        print("Car is moving......")
+
+my_car=Car()
+my_car.start()
+my_car.drive()
