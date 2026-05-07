27a433000c0c8d3f98786da36b65ed85113e5e88
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-07 19:27:38 +0530

    divya

---
+++ Python/Lecture 6 OOPS in Python/functional_programming.py
@@ -0,0 +1,30 @@
+"""
+Functional Programming:
+
+It is programming paradigm in which we try to bind everything
+in a pure mathematical functions
+
+Any functional programming language is expected to have the following concept
+
+1. pure function: These function should have main properties
+    a.They always produce the same output for same argument
+    irrespective of anything else.
+    b. They have no side effects that is they do not modify any argument or global variable or output
+
+
+2. Recursion : There are no "for" or "while" loops in functional 
+language . Iteration in Func. Prog. done through recursion
+
+
+
+3.Variables are Immutable: In functional programming we cannot modify
+a variable after its been initialized.But we can create new variable
+
+
+
+
+
+
+"""
+
+
