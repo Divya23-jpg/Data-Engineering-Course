01798ef748891b25dc817a4958dc20c41e3497b4
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-07 18:17:21 +0530

    divya

---
+++ Python/Lecture 6 OOPS in Python/polymorphrism.py
@@ -0,0 +1,13 @@
+
+"""
+
+# ?Polymerphrism
+
+This enables one interface to be used for diffrent data types and 
+methods to behave diffferently based on the context
+
+It al achivesd by Method Overriding and Method Overloading
+"""
+
+
+
