eb6dc8a2c34a8e63b53ce41aba22a92fd026deee
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-08 18:23:02 +0530

    divya

---
+++ Python/Lecture 6 OOPS in Python/method_overriding.py
@@ -0,0 +1,18 @@
+
+class DataEngineering:
+    def course(self):
+        return "Data Engeeniring Course"
+    
+
+class Python(DataEngineering):
+    def course(self):
+        return "Python Course"
+    
+
+class data(DataEngineering):
+    def course(self):
+        return "Data Course"
+    
+
+
+
--- Python/Lecture 6 OOPS in Python/polymorphrism.py
+++ Python/Lecture 6 OOPS in Python/polymorphrism.py
@@ -11,28 +11,39 @@ It al achivesd by Method Overriding and Method Overloading
 It allows developers to create more flexible code because, it makes object behaves similarly
 even when they have separate implementation
 
-
 Greek word: Poly + Morphism  = Multiple/Multi + froms
+
+Many forms of objects or functions depending on their usage and working.
+
+Functionality:
+
+1. The main role of Polymerphism in OOP is to ensure that different type of objects use the same methods or operators
 """
 
 
-# Example
-class Shape:
-    def area(self):
-        self.s=int(input("Enter side:"))
-        # self.b=int(input("Enter breadth:"))
+#1. Example
+# class Shape:
+#     def area(self):
+#         self.s=int(input("Enter side:"))
+#         # self.b=int(input("Enter breadth:"))
     
-# ! wrong output TRY
-class Square(Shape):
-    def area(self):
-        self.s*self.s
-        return f"Area of Square is {self.s}"
+# # ! wrong output TRY
+# class Square(Shape):
+#     def area(self):
+#         self.s*self.s
+#         return f"Area of Square is {self.s}"
     
 
-class Circle(Shape):
-    def area(self):
-        return " Area of Circle"
+# class Circle(Shape):
+#     def area(self):
+#         return " Area of Circle"
     
-shape=[Shape(),Square(),Circle()]
-for s in shape:
-    print(s.area())
+# shape=[Shape(),Square(),Circle()]
+# for s in shape:
+#     print(s.area())
+
+
+
+print(len("Divya"))
+print(len([1,2,3]))
+print(len((1,2,3,3)))
