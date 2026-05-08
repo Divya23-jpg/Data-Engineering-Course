19da595c7fa5cfd1d1bdcf4363e2d904527d34b4
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-08 18:41:13 +0530

    divya

--- Python/Lecture 6 OOPS in Python/method_overriding.py
+++ Python/Lecture 6 OOPS in Python/method_overriding.py
@@ -5,25 +5,32 @@ Benifits:
 2. Functions and methods of a class can be used for different purposes, which provide code reusability
 3. It offers code flexibility as child classes can be use method of parent classes wihtout changin code
 4. Multiple team can work on different parts of code without any conflict
-5. It allows
+5. It allows function to take various types of data input. The eleminates the need to separate functions for multiple data types like str,list,int etc
+
 
 
 """
-class DataEngineering:  #Parent class
-    def course(self):
-        return "Data Engeeniring Course"
+# class DataEngineering:  #Parent class
+#     def course(self):
+#         return "Data Engeeniring Course"
     
 
-class Python(DataEngineering):  # Child Class 
-    def course(self):           # Method Overriding 
-        return "Python Course"
+# class Python(DataEngineering):  # Child Class 
+#     def course(self):           # Method Overriding 
+#         return "Python Course"
     
 
-class data(DataEngineering):  # Child Class
-    def course(self):         # Method Overriding 
-        return "Data Course"
+# class data(DataEngineering):  # Child Class
+#     def course(self):         # Method Overriding 
+#         return "Data Course"
     
 
-for i in [Python(),data()]:
-    print(i.course())
+# for i in [Python(),data()]:
+#     print(i.course())
 
+
+# Polymerphrism in operators
+# ? IN Integer
+print("+ in Integers:",1+2)
+# ? IN String
+print("+ in String:","Divya"+"Kumawat")
