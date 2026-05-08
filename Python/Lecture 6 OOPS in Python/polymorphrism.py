155dfa6749509aabbca942459d726b37ea76b3a6
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-08 18:09:34 +0530

    divya

--- Python/Lecture 6 OOPS in Python/polymorphrism.py
+++ Python/Lecture 6 OOPS in Python/polymorphrism.py
@@ -7,6 +7,10 @@ This enables one interface to be used for diffrent data types and
 methods to behave diffferently based on the context
 
 It al achivesd by Method Overriding and Method Overloading
+
+It allows developers to create more flexible code because, it makes object behaves similarly
+even when they have separate implementation
+
 """
 
 
