d4b1d6a641c499a943db8434a707465edcce1918
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-08 18:33:43 +0530

    Divya

--- Python/Lecture 6 OOPS in Python/method_overriding.py
+++ Python/Lecture 6 OOPS in Python/method_overriding.py
@@ -1,4 +1,10 @@
+"""
+Benifits:
 
+1. It makes Simplified code as a single interface can handle multiple data type
+2. Functions and methods of a class can be used for different purposes, which provide code reusability
+3.It offers code flexibility as child classes can be use method of parent classes
+"""
 class DataEngineering:  #Parent class
     def course(self):
         return "Data Engeeniring Course"
@@ -14,5 +20,6 @@ class data(DataEngineering):  # Child Class
         return "Data Course"
     
 
-
+for i in [Python(),data()]:
+    print(i.course())
 
