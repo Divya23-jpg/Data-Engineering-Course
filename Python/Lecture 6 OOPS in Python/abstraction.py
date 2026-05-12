fcd1267cc8cd7490dae149608c30f7daa2b48f95
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-12 18:48:17 +0530

    divya

--- Python/Lecture 6 OOPS in Python/abstraction.py
+++ Python/Lecture 6 OOPS in Python/abstraction.py
@@ -14,3 +14,27 @@ Implemented using abstract base classes with the abc
 #         pass
 
 
+from abc import ABC , abstractmethod
+
+class Banking(ABC):
+    def database(self):
+        print("Connected with database")
+        
+    @abstractmethod
+    def display(self):
+        pass
+
+    @abstractmethod
+    def security(self):
+        pass
+
+class Mobile(Banking):
+    def security(self):
+        print("Biometric is imp")
+
+    # display method os imp because we make it  @abstractmethod without it mobile can not work
+    def display(self):
+        print("Display Mobile")
+
+m=Mobile()
+m.database()
