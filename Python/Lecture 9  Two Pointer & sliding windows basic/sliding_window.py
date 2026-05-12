b41666e35472fd4ba299eb6f0f96ba5d26f7dcc2
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-12 19:17:35 +0530

    divya

--- Python/Lecture 9  Two Pointer & sliding windows basic/sliding_window.py
+++ Python/Lecture 9  Two Pointer & sliding windows basic/sliding_window.py
@@ -1,8 +1,28 @@
 """
 
 Sliding Window is a Technique in DSA We can apply in list / array
+problem solved by sliding window
+1.Sub array,/sub string 
+2.Array/ String
+
+
+
+
+Types of  sliding window
+1. Fixed size window (k is given)
+2. Variable size window 
+"""
+
+# ! Find Sum of a Maximum sub array and  window size will be k=3
+
+"""
+1,2,3,4,5,6
+subarray: 123 , 234, 345, 456,
+Sum:       6,    9,  12,  15
 
 
 """
 
-# ! Sum of a Maximum sub array
+
+arr = [1, 2, 3, 4, 5,6]
+for i in range(7):
