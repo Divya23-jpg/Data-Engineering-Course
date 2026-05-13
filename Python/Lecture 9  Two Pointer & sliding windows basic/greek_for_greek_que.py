5e36a8d73304acbf68e5e4db5a22805e3186b95d
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-13 19:15:14 +0530

    divya

--- Python/Lecture 9  Two Pointer & sliding windows basic/greek_for_greek_que.py
+++ Python/Lecture 9  Two Pointer & sliding windows basic/greek_for_greek_que.py
@@ -1,5 +1,5 @@
 """
-print negative number and if positive window them print 0
+print 1st negative number and if positive window them print 0
 window=2
 input: arr[-8,2,3,-6,1]
 Output: [-8,0,-6,-6]
@@ -8,7 +8,8 @@ Output: [-8,0,-6,-6]
 def window(arr,k):
     n=len(arr)
     li=[]
-    
+
+
     
     return li
 
