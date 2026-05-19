54fce0d95e9c8abe02dc0355c55c43f92cfb2adb
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-19 19:31:36 +0530

    divya

---
+++ Python/Serach Algorithm/hash_function.py
@@ -0,0 +1,15 @@
+"""
+
+Built in function
+hash()
+1.hash values are immutable
+2.
+"""
+
+
+# !1.hash values are immutable
+print(hash(3.14))
+print(hash("Lorem"))
+print(hash("Lorem"))
+print(hash("divya"))
+print(hash("hello I am divya")==hash("hello I am divya"))
