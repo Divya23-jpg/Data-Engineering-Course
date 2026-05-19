383f5437d2644c62fc28e2077f8b77c850988b72
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-19 19:22:12 +0530

    divya

---
+++ Python/Serach Algorithm/task.py
@@ -0,0 +1,23 @@
+"""
+Create a dictionary with key:"name","age","city"
+value:"Gourav",20,"Vadodra"
+
+
+print the value for name 
+Add new key "Country" with the value "India"
+
+print the entire dictionary
+
+
+"""
+
+
+stu={
+    "name":"Gourav",
+    "age":20,
+    "city":"Vadodra"
+
+}
+print(stu["name"])
+stu["county"]="India"
+print(stu)
