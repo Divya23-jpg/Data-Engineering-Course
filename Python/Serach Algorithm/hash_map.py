92a6d4d28603599c9aaec705c89faa129862b575
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-19 18:54:27 +0530

    divya

--- Python/Serach Algorithm/hash_map.py
+++ Python/Serach Algorithm/hash_map.py
@@ -4,4 +4,26 @@
 Dictionary is a key value pair data type
 
 ! Hasing
+process to convert into hash table
+index=keys
+value=value
 """
+# d={"name":["Divya","rohan"],
+#     "roll_no":[10,34]}
+
+# print(d.keys())
+
+class Node:
+    def __init__(self,key,value):
+        self.key=key
+        self.value=value
+        self.next=None
+class HashTable:
+    def __init__(self,size,capacity):
+        self.size=size
+        self.capacity=capacity
+        self.table=None
+
+def _hash(self,key):
+    return hash(key) % self.capacity
+
