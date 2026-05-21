e798770bdf1771515690a430729f15120b69cebe
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 18:29:51 +0530

    divya

---
+++ Python/Lecture 12 Sorting/bubble_sort.py
@@ -0,0 +1,12 @@
+"""
+
+Bubble sort:
+    1.compare each pair of Adjacent element
+    2.if the 1st element is greater than second than swap
+    3. After each full pass, the largest unsorted element "Bubbles up" to its correct position
+    4.Repeat this process for the remaing unsorted portion to the list
+    5.Algorithm will stop early if there is no swap occurs in a pass,meaning array is already sorted
+
+
+"""
+
