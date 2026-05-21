67d1cae94cc7b1805ef78180c45508f41ca46b7a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 18:54:42 +0530

    divya

---
+++ Python/Lecture 12 Sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+"""
+Insertion sort
+
+    1.This sorting  algorithm works by iteratively each ele of an unsorted list into its correct position in a sorted portion of the list
+
+    Working:
+
+    1.start from the 2nd ele (1st is already sorted)
+    2.compare the current ele (key)
+    3.shift all larger elements one position to the right
+    4.Insert the key ele into its correct position
+    5.Repeat untill the entire array is sorted
+
+
+
+"""
--- Python/Lecture 12 Sorting/selection_sort.py
+++ Python/Lecture 12 Sorting/selection_sort.py
@@ -12,3 +12,28 @@ working:
     4. Repeat this proccess untill the entire array becomes sorted
 
 """
+def selection(arr,size):
+    for i in range(size):
+        min=i
+        for j in range(i+1,size):
+            if arr[j]<arr[min]:
+                min=j 
+            arr[i] , arr[min]= arr[min],arr[i]
+
+      
+# def selection(arr, size):
+#     for i in range(size):
+#         min_index = i
+#         for j in range(i+1, size):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         # Swap after finding the minimum in the remaining array
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+
+
+arr=[-2,45,0,11,-9,88,-97,-202,747]
+size=len(arr)
+selection(arr,size)
+print(arr)
