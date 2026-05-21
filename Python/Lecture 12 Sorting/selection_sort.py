0ad2c5b48c7e3689e4634e4bf9b62c01fc83cdf5
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 18:36:41 +0530

    divya

---
+++ Python/Lecture 12 Sorting/selection_sort.py
@@ -0,0 +1,14 @@
+"""
+
+Selection Sort
+    1.Comparison based technique .
+    2. It sorts array by repeatedly finding the smallest and largest elementin
+       the unsorted portion and place them in its correct position
+
+working:
+    1.start with the 1st ele and find smallest ele in entire array by iterating overwrite
+    2. Swap this smallest element with the 1st ele
+    3. Move to 2nd ele find 2nd smallest and place with 2nd ele
+    4. Repeat this proccess untill the entire array becomes sorted
+
+"""
