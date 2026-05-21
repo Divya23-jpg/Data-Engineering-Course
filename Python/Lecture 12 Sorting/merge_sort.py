e28e46e7da50a2df323197f9ddc5ad0aabfc2679
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 19:13:17 +0530

    divya

---
+++ Python/Lecture 12 Sorting/merge_sort.py
@@ -0,0 +1,10 @@
+"""
+
+Merge Sort:
+    1.It is a divide and conquer technique
+    2.It divides the input array in two halves, call itself for the two halves and then merge the two sorted halves
+    3.The merge() function is used for merging two sorting halves
+
+
+"""
+
