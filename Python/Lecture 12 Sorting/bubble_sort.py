6f94318dd95983bbffc3cb47724cab48f9b18ceb
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 18:42:33 +0530

    divya

--- Python/Lecture 12 Sorting/bubble_sort.py
+++ Python/Lecture 12 Sorting/bubble_sort.py
@@ -9,4 +9,15 @@ Bubble sort:
 
 
 """
+def bubble(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
 
+
+arr=[64,34,25,12,21,11,90]
+bubble(arr)
+print("Buuble Sort")
+print(arr)
