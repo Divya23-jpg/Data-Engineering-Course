e4d2705efe57ffa25f427d669738cd33122594b1
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-28 14:34:56 +0530

    divya

---
+++ Python/Lecture 12 Sorting/quick_sort.py
@@ -0,0 +1,16 @@
+"""
+
+Sorting algorithm  based on divide and conquer method
+that picks an element as a pivot and partitions the given array around the picked pivot by
+placing the  pivot and its correct position in the sorted array
+
+? Steps To Perform Quick sort
+1.Choosing pivot : Select an element from the array as the pivot element and choice of pivot can be anything
+2.partition the array: Re arranging everything around the pivot.
+After partitioning all elements smaller than pivot will be on the left side and all grater element will be in right
+3. Recursive call: Apply recusrsively the same process
+to get two potioned sub array
+    *Base case: The recursion would stop when there is only one elemeent left in the sub array,as single element in already sorted
+
+"""
+
