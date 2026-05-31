3f5e446fa587ae9a51499be326a6b51457888577
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-31 20:08:08 +0530

    divya

--- Python/Lecture 12 Sorting/File Handling/file_handling.py
+++ Python/Lecture 12 Sorting/File Handling/file_handling.py
@@ -3,9 +3,19 @@
 File Handling refers to any Operations like Reading, writting,closing
 etc through  a programming 
 
+.open(): Open file
+.close(): Close File
+.read(): Read whole data of the file
+.name(): Check file name
+.mode(): Check Mode of the file
+.closed(): Check file is closed or not 
+    ? Give 0 or 1
+.write(): Write in a file
+
+
 
 """
-
-file=open('hello.txt','r')
-content=file.read()
-print(content)
+# ! Open and Read File  
+file=open('hello.txt','r') # Open filen in a read mode
+content=file.read()  # Read whole content from the file
+print(content) # print content of the file
